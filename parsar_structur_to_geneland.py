0b3a5bd672e5c26c8fe9d176fae2852576cc08df
Francisco Pina Martins <f.pinamartins@gmail.com>
2015-06-04 16:36:04 +0100

    Changed program name in license header.

--- parsar_structur_to_geneland.py
+++ parsar_structur_to_geneland.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 # Copyright 2015 Telma G. Laurentino <telma.laurentino@gmail.com>
-# This file is part of clean_maf.
-# clean_maf is free software: you can redistribute it and/or modify
+# This file is part of parsar_structur_to_geneland.
+# parsar_structur_to_geneland is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
