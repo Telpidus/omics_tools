97578e7d94415ff14e59c8df97c8f66685a93d88
Telma G. Laurentino <telma.laurentino@gmail.com>
2016-01-22 15:36:01 +0000

    now bautifully commented

--- GESTE2SelEstim.sh
+++ GESTE2SelEstim.sh
@@ -2,13 +2,15 @@
 #co-authory Telma G. Laurentino(Telpidus); Francisco Pina-Martins(StuntsPT)
 #usage: bash script.sh <inputfile.geste> <#loci> <#pops> <output_file>
 
+# make sure script exists on error(careful with rm's)
 set -e
 
+# writes the header
 echo $3 > tmpfile
 echo $2 >> tmpfile
-echo "<#indv*2 pop1> <#indv*2 pop2> <#indv*2 pop3> <...>" >> tmpfile
+echo "<#indv*2 pop1> <#indv*2 pop2> <#indv*2 pop3> <...>" >> tmpfile # you have to input this by hand. sorry. 
 
-
+# gets the alleles freqs. from geste file and writes into SelEstim format
 touch teste
 for i in $(seq 1 $3); 
  do
